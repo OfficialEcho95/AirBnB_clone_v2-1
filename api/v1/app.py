4fd6a5f6b442f9f9c70bf606a76555dedde9331c
Emmanuel Chukwu <officialecho95@outlook.com>
2023-04-20 04:10:58 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,6 +23,14 @@ def teardown_app_context(exception):
 def error_404(error):
     '''Handles the 404 HTTP error code.'''
     return jsonify(error='Not found'), 404
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
 
 if __name__ == "__main__":
